256cbf4eac01632dcd59db264ac1ed836122c90e
Wendi <wendi0677@gmail.com>
2025-03-19 14:22:51 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "432px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
